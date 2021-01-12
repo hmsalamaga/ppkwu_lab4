526a729f4946de410290a84a6bc483a8e8751399
hubert-salamaga <hubert.salamaga@htdevelopers.com>
2021-01-12 14:51:14 +0100

    Add addresses and websites to vCards generated by page

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -1,39 +1,13 @@
 class FirmsController < ApplicationController
   def index
-    employee_response = Faraday.new.get("https://panoramafirm.pl/szukaj?k=#{params[:search_phrase]}").body.html_safe
-    page = Nokogiri::HTML.parse(employee_response)
-    @company_names = []
-    @emails = []
-    @phone_numbers = []
-    page = page.css('div#main-container').css('ul#company-list')
-
-
-    page.css('li.company-item').css('a.company-name').each do |company_name|
-      @company_names << company_name.text.strip
-    end
-
-
-    page.css('a.icon-envelope').each do |email|
-      @emails << email['data-company-email']
-    end
-    page.css('a.icon-telephone').each do |phone_number|
-      @phone_numbers << phone_number['title']
-    end
-
     @firms = []
-    @company_names.each_with_index do |company_name, index|
-      firm = Firm.new
-      firm.company_name = company_name
-      firm.email = @emails[index]
-      firm.phone_number = @phone_numbers[index]
-      @firms << firm
-    end
+    parse_website_to_firms if params[:search].present?
 
     render 'firms/index'
   end
 
   def vcard
-    firm = Firm.new()
+    firm = Firm.new(vcard_params)
 
     vcard = VCardigan.create(version: 4.0)
     vcard.charset 'iso-8859-1'
@@ -41,6 +15,10 @@ class FirmsController < ApplicationController
     vcard.fullname firm.company_name, charset: 'UTF-8'
     vcard.email firm.email
     vcard.tel firm.phone_number, type: ['work']
+    vcard.adr firm.address, type: ['work'], charset: 'UTF-8'
+    vcard[:item1].url firm.website
+    vcard[:item1].label 'HomePage'
+    puts vcard
 
     response.headers['Content-Type'] = 'text/x-vcard; charset=utf-8'
     send_data vcard.to_s, filename: "#{firm.company_name}.vcf"
@@ -48,7 +26,49 @@ class FirmsController < ApplicationController
 
   private
 
+  def parse_website_to_firms
+    employee_response = Faraday.new.get("https://panoramafirm.pl/szukaj?k=#{params[:search]}").body.html_safe
+    page = Nokogiri::HTML.parse(employee_response)
+    @company_names = []
+    @emails = []
+    @phone_numbers = []
+    @addresses = []
+    @websites = []
+    page = page.css('div#main-container').css('ul#company-list')
+
+
+    page.css('li.company-item').css('a.company-name').each do |company_name|
+      @company_names << company_name.text.strip
+    end
+
+    page.css('li.company-item').css('div.address').each do |address|
+      @addresses << address.text.strip
+    end
+
+    page.css('a.icon-website').each do |website|
+      @websites << website['href']
+    end
+
+    page.css('a.icon-envelope').each do |email|
+      @emails << email['data-company-email']
+    end
+
+    page.css('a.icon-telephone').each do |phone_number|
+      @phone_numbers << phone_number['title']
+    end
+
+    @company_names.each_with_index do |company_name, index|
+      firm = Firm.new
+      firm.company_name = company_name
+      firm.email = @emails[index]
+      firm.phone_number = @phone_numbers[index]
+      firm.website = @websites[index]
+      firm.address = @addresses[index]
+      @firms << firm
+    end
+  end
+
   def vcard_params
-    params.require(:firm).permit(:company_name, :phone_number, :email)
+    params.require(:firm).permit(:company_name, :phone_number, :email, :address, :website)
   end
 end
--- app/models/firm.rb
+++ app/models/firm.rb
@@ -3,13 +3,15 @@ class Firm
     args.each { |name, value| instance_variable_set("@#{name}", value) }
   end
 
-  attr_accessor :company_name, :email, :phone_number
+  attr_accessor :company_name, :email, :phone_number, :address, :website
 
   def to_h
     {
       company_name: company_name,
       email: email,
-      phone_number: phone_number
+      phone_number: phone_number,
+      address: address,
+      website: website
     }
   end
 end
